1d0ad8f912b2e1f20f44b00f31bc5a2e8ab9d30a
okste1234 <okste1234@gmail.com>
2024-05-21 01:58:57 +0100

    UPDATEA: about page implemented

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -3,16 +3,17 @@ import Hero from "@/components/shared/Hero";
 import MaxWidthContainer from "@/components/shared/MaxWidthContainer";
 
 export default function AboutPage() {
-  const title = `Hi there 👋 <br /> My name is Abdullahi Salihu, <br /> I'm a ReactJs Developer`;
+  const title = `Hi there 👋 <br /> My name is Adekunle Stephen Omorotimi, <br /> I'm a Software Developer`;
+  const description = `With years of experience in software development. creating custom applications and dApps that empower protocols and businesses through innovative solutions.
+  <br />
+  Software Developer || Blockchain & Smart Contract | Frontend Engineer | Protocol Research and Development`;
 
   return (
     <div className="w-full">
       <Hero
         showPath
         title={title}
-        description="With years of experience in web development, I specialize in
-            creating custom web applications that help businesses achieve their
-            goals by adapting innovative solutions."
+        description={description}
       />
 
       <MaxWidthContainer className="pt-16 md:pt-28">
